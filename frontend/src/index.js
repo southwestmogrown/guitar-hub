ae6f2efbba483aaacedae8db6d15798bfaeb5ae8
southwestmogrown <mail@southwestmogrown.com>
2021-06-17 10:55:21 -0500

    finished and tested thunk logic for login page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import App from './App';
 
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -16,6 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
   restoreCSRF();
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 };
 
 function Root() {
