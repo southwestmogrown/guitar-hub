4aa4019b51ed88d5ab597480909613fa9b572d33
southwestmogrown <mail@southwestmogrown.com>
2021-06-17 10:23:45 -0500

    finished front end setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,11 +8,14 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
-  window.store = store
+  restoreCSRF();
+  window.csrfFetch = csrfFetch;
+  window.store = store;
 };
 
 function Root() {
